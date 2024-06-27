31600e8b83d64ce396d787d43c88cba734f76baa
ereem-1 <d.ereomin98@gmail.com>
2024-06-27 10:11:03 +0300

    1st patch. Some functions fixes. Added anotations, comments. Fixed some html files.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
-from scrapper import dotaBuffScrapping, heroes_links, counterBuffScrapper
+from scrapper import dotaBuffScrapping, heroes_links, \
+                    counterBuffScrapper, titles, counterpicks
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -24,6 +25,9 @@ def get_hero_info(request: Request, item_id: str):
     return templates.TemplateResponse("info_page/info.html",
                                       {"request": request,
                                        "hero_name": item_id,
+                                       "titles": titles,
+                                       "length": len(dotaBuffScrapping
+                                                     (item_id)[0]),
                                        "statistics":
                                        dotaBuffScrapping(item_id)})
 
@@ -33,5 +37,8 @@ def get_hero_counters(request: Request, item_id: str):
     return templates.TemplateResponse("counter_page/counter.html",
                                       {"request": request,
                                        "hero_name": item_id,
+                                       "counterpicks": counterpicks,
+                                       "length": len(counterBuffScrapper
+                                                     (item_id)[0]),
                                        "statistics":
                                        counterBuffScrapper(item_id)})
--- test.py
+++ test.py
@@ -10,25 +10,22 @@ headers = {
     'Gecko/20100101 Firefox/115.0',
            }
 
-LIMIT = 18
-LIMIT_LANES = 4
-SPLITTER_HERO = 6
-SPLITTER_COUNTER = 4
-titles = ['Lane', 'Presence', 'Win Rate', 'KDA', 'GPM', 'XPM']
-counterpicks = ['Hero', 'Disadvantage', 'Win Rate', 'Matches']
 response_site = requests.get(url, headers=headers)
 response_lanes = requests.get(f'{url}/lanes', headers=headers)
+
 bs_site = Soup(response_site.text, 'html.parser')
 bs_lanes = Soup(response_lanes.text, 'html.parser')
+LIMIT = 18
 
-'''Функция групировки/разбиения списка на одинаковое количество списков'''
+
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
 
-'''Функция, которая при выполнении получает список героев'''
-def heroes_list() -> list:
+
+def heroes_list():
     temp = bs_site.find_all('div', 'tw-flex tw-flex-col tw-gap-0')
+
     heroes = []
     for i in temp:
         hero = i.find_next('div').text
@@ -37,8 +34,8 @@ def heroes_list() -> list:
         heroes[i] = heroes[i].lower().replace(' ', '-')
     return heroes
 
-'''Функция, которая получает список линий'''
-def lanes_list() -> list:
+
+def lanes_list():
     mid_lane = bs_lanes.find('div', class_='filter')\
                     .find('div', class_='nav').find_next('nav')\
                     .find_next('ul').find_next('li', class_='active')
@@ -46,14 +43,14 @@ def lanes_list() -> list:
         .find('div', class_='nav')\
         .find_next('nav').find_next('ul')\
         .find_next('li', class_='active')\
-        .find_all_next('li', class_='', limit=LIMIT_LANES)
+        .find_all_next('li', class_='', limit=4)
     lanes = [mid_lane.text]
     for i in other_lanes:
         lanes.append(i.text)
     return lanes
 
-'''Функция, которая на выходе возвращает статистику героя на линиях'''
-def dotaBuffScrapping(item_id: str) -> list:
+
+def dotaBuffScrapping(item_id):
     response_hero = requests.get(f'{url}/{item_id}', headers=headers)
     bs_hero = Soup(response_hero.text, 'html.parser')
     temp = bs_hero.find('div', class_='col-8').find('section')\
@@ -67,24 +64,23 @@ def dotaBuffScrapping(item_id: str) -> list:
     for i in range(len(heroe_lane)):
         if heroe_lane[i][0] in lanes_list():
             heroe_lane2 += heroe_lane[i]
-    heroes_statistic = [heroe_lane2[i:i + SPLITTER_HERO]
-                        for i in range(0, len(heroe_lane2), SPLITTER_HERO)]
+    splitter = 6
+    heroes_statistic = [heroe_lane2[i:i + splitter]
+                        for i in range(0, len(heroe_lane2), splitter)]
     return heroes_statistic
 
 
-''''Функция, котоорая создает словарь,
-где ключ это герой, а значение - ссылка на него'''
-def heroes_links() -> dict:
+def heroes_links():
     dictionary = {}
+
     for i in range(len(heroes_list())):
         dictionary[heroes_list()[i]] = f'127.0.0.1:8000/info/\
         {heroes_list()[i]}'
     return dictionary
 
 
-'''Функция, которая возвращает статистику контрпиков героя'''
-def counterBuffScrapper(item_id: str) -> list:
-    c = []
+def counterBuffScrapper(item_id):
+    c =[]
     response_counter = requests.get(f'{url}/{item_id}', headers=headers)
     bs_counter = Soup(response_counter.text, 'html.parser')
     temp = bs_counter.find('div', class_='col-8').section.next_sibling\
@@ -93,12 +89,72 @@ def counterBuffScrapper(item_id: str) -> list:
         .next_sibling.next_sibling\
         .next_sibling.next_sibling\
         .next_sibling.tbody.tr
+    # temp = bs_counter.find_next_sibling("tr")
     nextSiblings = temp.next_siblings
     for nextSibling in nextSiblings:
+        # temp.next_sibling.next_sibling
         for next in nextSibling:
             c.append(next.text)
-    for i in c: # Удаление пробелов из списка контрпиков
+    for i in c:
         if i == '':
             c.remove(i)
-    counter_statistic = list(grouper(c, SPLITTER_COUNTER, fillvalue=None))
+    counter_statistic = list(grouper(c, 4, fillvalue=None))
     return counter_statistic
+
+            # print(next.text)
+    # return temp.text
+    # heroes = []
+    # for i in temp.find_all('td'):
+    #     heroes.append(i.text)
+
+    # heroe_lanes = list(grouper(heroes, 5, fillvalue=None))
+    # splitter = 10
+    # heroes_statistic = [heroe_lanes[i:i + splitter] for i in
+    #                     range(0, len(heroe_lanes), splitter)]
+    # return heroes_statistic
+
+print(counterBuffScrapper('io'))
+# counterBuffScrapper('io')
+# uniq = [1,2,3,4,5]
+# fifa = ['a','b','c','d','e']
+# uniq_and_fifa = dict(zip(uniq, fifa))
+# print(uniq_and_fifa)
+titles = ['Lane', 'Presence', 'Win Rate', 'KDA', 'GPM', 'XPM']
+# titles_and_statistics = dict(zip(titles, dotaBuffScrapping('lifestealer')))
+# print(titles_and_statistics)
+# print(dotaBuffScrapping('io'))
+# print(len(dotaBuffScrapping('io')[0]))
+
+
+def statistics_dict(item_id):
+    for a in range(len(dotaBuffScrapping(item_id))):
+            b = dict(zip(titles, dotaBuffScrapping(item_id)[a]))
+            c = dict(zip(titles, dotaBuffScrapping(item_id)[a+1]))
+            d = dict(zip(titles, dotaBuffScrapping(item_id)[a+2]))
+        # dotaBuffScrapping(item_id)
+        # break
+    return b, c, d
+# for i in dotaBuffScrapping('io')[0]:
+#     print(i)
+# print(dotaBuffScrapping('io'))
+# print(statistics_dict('abaddon'))
+# statistics_dict('io')
+# print(b)
+# def counterBuffScrapper(item_id):
+#     response_counter = requests.get(f'{url}/{item_id}', headers=headers)
+#     bs_counter = Soup(response_counter.text, 'html.parser')
+#     temp = bs_counter.find('div', class_='col-8').section.next_sibling\
+#         .next_sibling\
+#         .next_sibling.next_sibling\
+#         .next_sibling.next_sibling\
+#         .next_sibling.next_sibling\
+#         .next_sibling
+#     heroes = []
+#     for i in temp.find_all('td'):
+#         heroes.append(i.text)
+
+#     heroe_lanes = list(grouper(heroes, 5, fillvalue=None))
+#     splitter = 10
+#     heroes_statistic = [heroe_lanes[i:i + splitter] for i in
+#                         range(0, len(heroe_lanes), splitter)]
+#     return heroes_statistic
